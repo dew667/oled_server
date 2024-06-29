4b1a4e83d5c2b789d56922272cfcadb8ea37714a
dew667 <dew_wang667@126.com>
2024-06-29 13:46:54 +0800

    oled server 首次上传

---
+++ http_server.py
@@ -0,0 +1,42 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+from display import drawText
+import json
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        query_params = parse_qs(parsed_path.query)
+
+        msg = query_params.get('msg', [''])[0]
+        
+        drawText(msg)
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        
+        response = {
+            'message': 'Received GET request',
+            'body': msg
+        }
+        
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
